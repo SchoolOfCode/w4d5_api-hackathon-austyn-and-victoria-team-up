22c8904d5adfea0ec01c3517d6076a44087f4b36
atemp678 <ajt2158@columbia.edu>
2021-12-10 12:18:46 +0000

    added recipes and app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from "express";
 
 import { html } from "./config.js";
 
+import {
+  getRecipes,
+  getRecipeByID,
+  createRecipe,
+  updateRecipeByID,
+  deleteRecipeByID,
+} from "..models/recipes.js";
+
 const app = express();
 const PORT = 3000;
 
@@ -14,10 +22,34 @@ app.get("/", function (req, res) {
 });
 
 /** YOUR ROUTES GO HERE */
+app.get("/recipes", async function (req, res) {
+  const allRecipes = await getRecipes();
+  res.json(allRecipes);
+});
+
+app.get("/recipes/:id", async function (req, res) {
+  const recipeByID = await getRecipeByID();
+  res.json(recipeByID);
+});
+
+app.post("/recipes", async function (req, res) {
+  const newRecipe = req.body;
+  newRecipe = await createRecipe();
+  res.json(newRecipe);
+});
+
+app.patch("/recipes/:id", async function (req, res) {
+  const updatedRecipe = await updateRecipeByID(req.params.id, req.body);
+  res.json(updatedRecipe);
+});
+
+app.delete("/recipes/:id", async function (req, res) {
+  const deletedRecipe = await deleteRecipeByID(req.params.id);
+  res.json(deletedRecipe);
+});
 
 /** END OF YOUR ROUTES */
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
--- models/recipes.js
+++ models/recipes.js
@@ -1,39 +1,43 @@
 import recipes from "../data/recipes.js";
 
-
 // getRecipeByID should return the particular recipe we are looking for
 // createRecipe should add a recipe to the collection and return the new recipe
 // updateRecipeByID should replace the recipe at a certain ID with an updated version and return the new recipe
 // deleteRecipeByID should remove the specific recipe from the collection, and return the deleted recipe
 
-
 // GET ALL RECIPES
 // getRecipes should return an array of all recipes
-export function getRecipes() {
-  let allRecipes = recipes;
-  return allRecipes;
+export async function getRecipes() {
+  return recipes;
 }
 
 // GET A RECIPE BY ID
-export function getRecipeByID(id) {
-  let recipesById = recipes;
+export async function getRecipeByID(id) {
   const recipeById = recipes.find(({ id }) => id === requestId);
   if (recipeById) {
     return recipeById;
-
+  }
 }
 
 // CREATE A RECIPE
-export function createRecipe(recipe) {
-  let newRecipe = {
-    id:
-  }
-   let allRecipes = recipes;
-   allRecipes.push(newRecipe)
+export async function createRecipe(recipe) {
+  recipes.push(recipe);
 }
 
 // UPDATE A RECIPE BY ID
-export function updateRecipeByID(id) {}
+export async function updateRecipeByID(id) {
+
+    const newRecipe = recipes.find(({ id }) => id === newId);
+
+  if (recipeByID) {
+    recipes.push({});
+    let recipeByID = id;
+  }
+  return newRecipe;
+}
 
 // DELETE A RECIPE BY ID
-export function deleteRecipeByID(id) {}
+export async function deleteRecipeByID(id) {
+  recipes.splice(id);
+  return recipes;
+}
